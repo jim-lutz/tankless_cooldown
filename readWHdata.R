c13a1a29294f0bdcf7a54f773996aab2fc76143b
Jim Lutz <jdlutz@hotwaterresearch.net>
2017-07-31 16:52:59 -0700

    trouble doing a nonlinear fit in R

--- readWHdata.R
+++ readWHdata.R
@@ -27,6 +27,25 @@ DT_draws <- DT_draws[model != "Navien NP-240",] # that leave 46 draws, 5 models
 # scatter plot, for verification
 ggplot(DT_draws, aes(t_Delay, f_cooldown, colour = model)) + geom_point()
 
+# try finding a non-linear fit
+fit <- nls(formula=f_cooldown ~ exp(-t_Delay/TC), 
+    data = DT_draws
+    )
+
+summary(fit)
+str(fit)
+str(summary(fit))
+TC.est <- summary(fit)$coefficients[[1]]
+
+DT_est <- data.table(t_Delay=0:600)
+DT_est[,f_cooldown:= exp(-t_Delay/TC.est)]
+
+
+# with non-linear fit
+ggplot() +
+  geom_point(data=DT_draws, aes(t_Delay, f_cooldown, colour = model)) + 
+  geom_line(data=DT_est,aes(t_Delay, f_cooldown))
+
 # save data.table
 save(DT_WHs , file=paste0(wd_data,"DT_WHs.RData"))
 
